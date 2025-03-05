68f202b329c8c608d25c4aefff06ea8a027fac04
Ichahal <83983971+Ichahal@users.noreply.github.com>
2025-03-05 12:44:18 -0500

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Aman  Chahal on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
